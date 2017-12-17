b413216eb2bb767a39c9f49dc81de88d5c3267e2
v-lavs <lavskavanda@gmail.com>
2017-12-17 11:15:29 +0200

    add ;

--- source-js/main.js
+++ source-js/main.js
@@ -1,43 +1,43 @@
 // sum
 function sum (a, b) {
-  var result = a + b
-  console.log(result)
-  return result
+  var result = a + b;
+  console.log(result);
+  return result;
 }
 
 // subtract
 function subtraction (a, b) {
-  var result = a - b
-  console.log(result)
-  return result
+  var result = a - b;
+  console.log(result);
+  return result;
 }
 
 //multiplication
 function multiplication (a, b) {
-  var result = a * b
-  console.log(result)
-  return result
+  var result = a * b;
+  console.log(result);
+  return result;
 }
 
 //division
 function division (a, b) {
-  var result = (b === 0) ? 'Error' : a / b
-  console.log(result)
-  return result
+  var result = (b === 0) ? 'Error' : a / b;
+  console.log(result);
+  return result;
 }
 
 //pow
 function pow (number, degree) {
-  var result = number
+  var result = number;
   if (degree >= 1) {
     for (var i = 1; i < degree; i++) {
-      result *= number
+      result *= number;
     }
-    console.log(result)
-    return result
+    console.log(result);
+    return result;
   } else {
-    console.log(1)
-    return 1
+    console.log(1);
+    return 1;
   }
 }
 
@@ -45,41 +45,41 @@ pow(3, 2)
 
 //sqrt
 function sqrt (number) {
-  var result = Math.sqrt(number)
-  console.log(result)
-  return result
+  var result = Math.sqrt(number);
+  console.log(result);
+  return result;
 }
 
 //cos
 function cos (number) {
-  var result = Math.cos(number)
-  console.log(result)
-  return result
+  var result = Math.cos(number);
+  console.log(result);
+  return result;
 }
 
 //sin
 function sin (number) {
-  var result = Math.sin(number)
-  console.log(result)
-  return result
+  var result = Math.sin(number);
+  console.log(result);
+  return result;
 }
 
 //factorial
 function factorial (number) {
   //!5 = 5*4*3*2*1
-  var result = number
+  var result = number;
   if (number >= 1) {
     for (var i = number; i > 1; i--) {
-      result *= i - 1
+      result *= i - 1;
     }
   } else if (number === 0) {
-    result = 1
+    result = 1;
   }
   else {
-    result = 'number must be positive'
+    result = 'number must be positive';
   }
-  console.log(result)
-  return result
+  console.log(result);
+  return result;
 }
 
 
